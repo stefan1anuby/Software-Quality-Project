1b7dbd633958fbfbd802addaab2175e63daae602
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2025-05-02 17:15:20 +0300

    timetables and teachers pages

--- app/main.py
+++ app/main.py
@@ -3,8 +3,18 @@ from app.api.v1 import timetable
 from contextlib import asynccontextmanager
 from app.core.database import create_db_and_tables
 from app.seeder.seed_data import seed_data
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            # or ["http://localhost:8000", "http://127.0.0.1:8000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
@@ -12,4 +22,5 @@ def on_startup():
     seed_data()
 
 app.include_router(timetable.router, prefix="/api/v1/timetable")
+# app.mount("/", StaticFiles(directory="app/frontend", html=True), name="frontend")
 
